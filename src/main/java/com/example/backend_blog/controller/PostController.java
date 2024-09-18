ac57b967b1492c340defeda833436fd3334b7b35
wedson97 <wedsonsilva025@gmail.com>
2024-09-18 20:04:32 -0300

    PUT e DELETE de posts

--- src/main/java/com/example/backend_blog/controller/PostController.java
+++ src/main/java/com/example/backend_blog/controller/PostController.java
@@ -1,10 +1,15 @@
 package com.example.backend_blog.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,7 +38,27 @@ public class PostController {
 	
 	@PostMapping
 	public void salvarPost(@RequestBody PostRequestDTO data) {
-		Post usuarioData = new Post(data,usuarioRepository);
-		postrepository.save(usuarioData);
+		Post postData = new Post(data,usuarioRepository);
+		postrepository.save(postData);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Post> editarPost(@PathVariable Long id,@RequestBody PostRequestDTO data ) {
+		Optional<Post> post_por_id = postrepository.findById(id);
+		Post post = post_por_id.get();
+        post.setTitulo(data.titulo());
+        post.setConteudo(data.conteudo());
+        post.setImagem_url(data.imagem_url());
+        postrepository.save(post);
+        return ResponseEntity.ok(post);
+		
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Post> excluirPost(@PathVariable Long id){
+		Optional<Post> post_por_id = postrepository.findById(id);
+		Post post = post_por_id.get();
+		postrepository.delete(post);
+		return ResponseEntity.ok(post);
 	}
 }
